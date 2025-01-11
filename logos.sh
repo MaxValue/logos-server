c3347e503511c744eb6b67f5fe268e687f4006ce
Max Fuxjäger <maxvalue.git@gmail.com>
2025-01-11 20:50:20 +0100

    Initial commit

---
+++ logos.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+logosRootPath=${1:-$PWD}
+defaultGroup=${2:-AT}
+targetStyle=${3:-default}
+extensionPreference=${4:-'svg webp png jpg ico'}
+
+cd $logosRootPath
+find . -type l -delete
+for groupFolder in $(ls -d */); do
+  cd "$groupFolder"
+  for logoStyleVariant in $(find . -type f | sed 's#.*/##; s#[.][^.]*$##' | sort | uniq); do
+    for extension in $extensionPreference; do
+      if [ -f $logoStyleVariant.$extension ]; then
+        ln -s -F -f $logoStyleVariant.$extension $logoStyleVariant
+        break
+      fi
+    done
+  done
+  for logoVariant in ${targetStyle}_*; do
+    ln -s -F -f $logoVariant logo_${logoVariant/#`echo $targetStyle`_/}
+  done
+  cd ..
+done
+
+ln -s -F -f $defaultGroup/* ./
